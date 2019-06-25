2569cadc21450ddc5b687514238216ba601f02fe
rodrigoGit-2014 <rodrigocaceresv@gmail.com>
2019-06-25 14:51:49 -0400

    new file

---
+++ LargestSubstring.java
@@ -0,0 +1,45 @@
+package com.example.demo.pangram.exercice;
+
+public class LargestSubstring {
+
+    public static void main(String[] args) {
+
+        char[] booleans = new char[]{'#', '*', '#', '#', '*', '*', '*', '#', '#', '#', '*', '*', '#'};
+        String xx = largestSubstring(booleans);
+        System.out.println(xx);
+    }
+
+    public static String largestSubstring(char[] line) {
+
+        int duplicateCount = 1, auxDuplicateCount = 0, start = 0, end = 0;
+
+        boolean visited = false;
+
+        for (int ind = 1; ind < line.length; ind++) {
+
+            if (line[ind - 1] != line[ind]) {
+                if (duplicateCount > auxDuplicateCount) {
+                    end = ind;
+                    start = (ind - duplicateCount) - (duplicateCount - 1);
+                }
+                auxDuplicateCount = duplicateCount;
+                duplicateCount = 0;
+
+            }
+
+            duplicateCount++;
+        }
+
+        return buildString(start, end, line);
+    }
+
+
+    private static String buildString(int start, int end, char[] line) {
+        String substring = new String();
+        for (int i = start; i <= end; i++) {
+            substring += line[i];
+        }
+        return substring;
+    }
+}
+
